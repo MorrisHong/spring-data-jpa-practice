6e4acef03f6d90f38987bb3994ee5f92760fc0e0
gracelove <govlmo91@gmail.com>
2019-11-29 21:57:00 +0900

    순수 JPA기반 리포지토리

--- src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -30,4 +30,35 @@ class MemberJpaRepositoryTest {
         assertThat(findMember).isEqualTo(member);
     }
 
+    @Test
+    void basicCRUD() {
+        final Member member1 = new Member("member1");
+        final Member member2 = new Member("member2");
+
+        //create
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        //read
+        final Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
+        final Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+
+        assertThat(findMember1.getUsername()).isEqualTo(member1.getUsername());
+        assertThat(findMember2.getUsername()).isEqualTo(member2.getUsername());
+
+        assertThat(memberJpaRepository.count()).isEqualTo(2);
+
+        //update (dirty check)
+        final String updateFindMember1Username = "updateFindMember1Username";
+        final String updateFindMember2Username = "updateFindMember2Username";
+        findMember1.setUsername(updateFindMember1Username);
+        findMember2.setUsername(updateFindMember2Username);
+        assertThat(findMember1.getUsername()).isEqualTo(updateFindMember1Username);
+        assertThat(findMember2.getUsername()).isEqualTo(updateFindMember2Username);
+
+        //delete
+        memberJpaRepository.delete(findMember1);
+        assertThat(memberJpaRepository.count()).isEqualTo(1);
+    }
+
 }

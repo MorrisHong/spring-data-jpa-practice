7e9d78871ceae8957682af09651ebcb90aa18b26
gracelove <govlmo91@gmail.com>
2019-11-30 18:20:01 +0900

    memberRepository Test update

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -3,16 +3,11 @@ package study.datajpa.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
-import java.util.List;
-import java.util.Optional;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 @Transactional
@@ -32,6 +27,37 @@ class MemberRepositoryTest {
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
         assertThat(findMember).isEqualTo(member);
+    }
+
+    @Test
+    void basicCRUD() {
+        final Member member1 = new Member("member1");
+        final Member member2 = new Member("member2");
+
+        //create
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        //read
+        final Member findMember1 = memberRepository.findById(member1.getId()).get();
+        final Member findMember2 = memberRepository.findById(member2.getId()).get();
+
+        assertThat(findMember1.getUsername()).isEqualTo(member1.getUsername());
+        assertThat(findMember2.getUsername()).isEqualTo(member2.getUsername());
+
+        assertThat(memberRepository.count()).isEqualTo(2);
+
+        //update (dirty check)
+        final String updateFindMember1Username = "updateFindMember1Username";
+        final String updateFindMember2Username = "updateFindMember2Username";
+        findMember1.setUsername(updateFindMember1Username);
+        findMember2.setUsername(updateFindMember2Username);
+        assertThat(findMember1.getUsername()).isEqualTo(updateFindMember1Username);
+        assertThat(findMember2.getUsername()).isEqualTo(updateFindMember2Username);
+
+        //delete
+        memberRepository.delete(findMember1);
+        assertThat(memberRepository.count()).isEqualTo(1);
     }
 
 }

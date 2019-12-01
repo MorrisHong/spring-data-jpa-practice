663ff37e4bdffda124153727c99d2d7fed62bc8b
gracelove <govlmo91@gmail.com>
2019-12-02 05:01:08 +0900

    페이징과 정렬

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -1,9 +1,13 @@
 package study.datajpa.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
 
@@ -26,9 +30,21 @@ public class MemberController {
         return member.getUsername();
     }
 
+    /**
+     * http://localhost:8080/members?page=0&size=3&sort=id,desc
+     */
+    @GetMapping("/members")
+    public Page<MemberDto> list(@PageableDefault(size = 5, sort = "username") Pageable pageable) {
+        final Page<Member> page = memberRepository.findAll(pageable);
+        return page.map(MemberDto::new);
+    }
+
     @PostConstruct
     public void init() {
-        Member member = new Member("member1");
-        memberRepository.save(member);
+        for(int i = 0; i <= 100; i++) {
+            Member member = new Member("member"+i, i);
+            memberRepository.save(member);
+        }
+
     }
 }

80c36452aa5d1497f125e6fd1e89a8c2d36faf95
gracelove <govlmo91@gmail.com>
2020-02-10 14:23:50 +0900

    순수 jpa기반 리포지토리 만들기

--- src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -7,6 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -29,5 +31,33 @@ class MemberJpaRepositoryTest {
         assertEquals(findMember.getId(), member.getId());
         assertEquals(findMember.getUsername(), member.getUsername());
      }
+
+     @Test
+     @Transactional
+     void basic_crud() throws Exception {
+         //given
+         Member member1 = new Member("member1");
+         Member member2 = new Member("member2");
+         memberJpaRepository.save(member1);
+         memberJpaRepository.save(member2);
+
+
+         //when
+         Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
+         Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+
+         List<Member> findMembers = memberJpaRepository.findAll();
+         //then
+         assertEquals(findMember1.getId(), member1.getId());
+         assertEquals(findMember2.getId(), member2.getId());
+
+         assertEquals(2, findMembers.size());
+         assertEquals(2, memberJpaRepository.count());
+
+         memberJpaRepository.delete(member1);
+         memberJpaRepository.delete(member2);
+
+         assertEquals(0, memberJpaRepository.count());
+      }
 
 }
